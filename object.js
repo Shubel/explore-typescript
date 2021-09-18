ddd3279634c834c9f9e1a5f65a829c1b39f1fb53
Sunny <shubelislam.diu@gmail.com>
2021-09-18 13:46:14 +0600

    object added

---
+++ object.js
@@ -0,0 +1,19 @@
+var city = "Dhaka";
+var fruits = ["mango", "banana", "apple"];
+var student = {
+    name: "Kamal Atartuk",
+    age: 115,
+    salary: 4500
+};
+var employee = {
+    name: "Solaiman",
+    age: 29,
+    salary: 1000000
+};
+function getSalary(player) {
+    return player.salary;
+}
+getSalary({ name: "kuddus", salary: 50 });
+function getSalary2(player) {
+    return player.salary;
+}
