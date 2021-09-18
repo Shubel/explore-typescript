d940044cefb445b9b5b95441d0c679ae1a0e522a
Sunny <shubelislam.diu@gmail.com>
2021-09-18 13:45:18 +0600

    interface added

---
+++ interface.js
@@ -0,0 +1,20 @@
+var messy = {
+    name: "L Messy",
+    club: "Abahoni",
+    salary: 12000,
+    wife: "Nosimon ara begum"
+};
+var hannan = {
+    name: "A. Hannan",
+    club: "Kolabagan",
+    salary: 2000
+};
+var zuku = {
+    name: "Zukerberg",
+    salary: 1100,
+    age: 31,
+    designation: "Facebooker",
+    language: "js",
+    codeEditor: "VS Code",
+    typingSpeed: 65
+};
